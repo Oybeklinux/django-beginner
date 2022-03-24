164ceb122c9969819fffef6c61bcce7e5ae4b097
Oybeklinux <Oybeklinux@gmail.com>
2022-03-24 14:34:17 +0500

    3.5

--- dars_1/projects/admin.py
+++ dars_1/projects/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Project
+from .models import *
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Tag)
+admin.site.register(Review)
--- dars_1/projects/models.py
+++ dars_1/projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Project(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(null=True, blank=True) #default=""
@@ -11,8 +12,34 @@ class Project(models.Model):
     vote_count = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    tag = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("+", "Ijobiy"),
+        ("-", "Salbiy")
+    )
+    body = models.TextField(default='')
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.value
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
 
