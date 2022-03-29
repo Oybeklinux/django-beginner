93215decca767accdd1c401ecf43432bf8f453db
Oybeklinux <Oybeklinux@gmail.com>
2022-03-29 12:03:26 +0500

    lesson 4.1 was added

--- portfolio/projects/urls.py
+++ portfolio/projects/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 # 127.0.0.1:8000/
 urlpatterns = [
+    path("", projects, name="projects"),
     path("projects/", projects, name="projects"),
     path("projects/<uuid:id>", project,  name="project"),
 ]
