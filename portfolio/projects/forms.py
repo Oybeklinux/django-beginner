6d9438f877885530edb00173cc1359866f4a1d1e
Oybeklinux <Oybeklinux@gmail.com>
2022-04-18 13:53:56 +0500

    6.5

--- portfolio/projects/forms.py
+++ portfolio/projects/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Project
 
@@ -6,3 +7,14 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
+
+        widgets = {
+            'tag': forms.CheckboxSelectMultiple()
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+
+        for key, field in self.fields.items():
+            field.widget.attrs.update({"class": "input input--text"})
