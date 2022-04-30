a6eaf138941de2701a5380cacf7aafe026bb3e3e
Oybeklinux <Oybeklinux@gmail.com>
2022-04-30 11:37:58 +0500

    9.3

--- portfolio/users/urls.py
+++ portfolio/users/urls.py
@@ -4,5 +4,6 @@ from .views import *
 # 127.0.0.1:8000/
 urlpatterns = [
     path("", profiles, name="profiles"),
+    path("login/", login_user, name="login"),
     path("profiles/<str:id>", profile, name="profile"),
 ]
--- portfolio/users/views.py
+++ portfolio/users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Profil
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def profiles(request):
@@ -15,3 +16,22 @@ def profile(request, id):
         "user": user
     }
     return render(request, 'users/profile.html', context)
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+        
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('Bunday login va parol mavjud emas')
+
+
+    return render(request, "users/login.html")
