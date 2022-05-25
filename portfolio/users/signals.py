0e7c0fb740d7e12822b37a7884e653280c01581e
Oybeklinux <Oybeklinux@gmail.com>
2022-05-25 15:53:29 +0500

    10.4

--- portfolio/users/signals.py
+++ portfolio/users/signals.py
@@ -10,14 +10,17 @@ def create_profile(sender, instance, created, **kwargs):
         Profil.objects.create(
             user=user
         )
-    else:
-        try:
-            profile = Profil.objects.get(user=user)
-            profile.email = user.email
-            profile.name = f"{user.first_name} {user.last_name}"
-            profile.save()
-        except:
-            pass
+
+
+@receiver(post_save, sender=Profil)
+def user_edit(sender, instance, created, **kwargs):
+    user = instance.user
+    first_last_name = instance.name.split(" ")
+    if instance.name and len(first_last_name) == 2:
+        user.first_name, user.last_name = first_last_name
+    if instance.email:
+        user.email = instance.email
+    user.save()
 
 
 @receiver(post_delete, sender=Profil)
